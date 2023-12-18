76f1db58c6787b214da11bc5d5b1e33773dad63d
Bernhard Birnbaum <bernhard.birnbaum@ovgu.de>
2023-12-18 20:02:04 +0100

    sheet 9

--- sheet_9/src/Task94.hs
+++ sheet_9/src/Task94.hs
@@ -1,10 +1,22 @@
-module Task94 where
-    State s a = State runState :: s -> (a, s)
+module Main where
+    import Data.Map
+    import System.IO
 
-    step :: State Field String
-    walk :: Field -> (String, Field)
+    data Direction = Left | Right | Up | Down
+
+    data Ant = Ant (Int, Int) Direction
+    data Field = Field (Map (Int, Int) Bool) Ant
+
+    --State s a = State runState :: s -> (a, s)
+
+
+    --step :: State Field String
+    --walk :: Field -> (String, Field)
 
     main :: IO ()
     main = do
-        g <- fromList [((i, j), False) | i <- [10..10], j <- [10..10]]
-        f st (runState (replicateM 10 step) (Field g (Ant (0, 0) Up)))
+        let g = fromList [((i, j), False) | i <- [1..10], j <- [1..10]]
+        let f = Field g (Ant (0, 0) Up)
+        putStrLn (show g)
+        hFlush stdout
+        --f st (runState (replicateM 10 step) (Field g (Ant (0, 0) Up)))
